72e9efece3438ab13d0f082d6c2fd21198dc3202
David Pinchao <sangeeky@outlook.com>
2021-04-08 01:26:24 -0500

    Fix: Modify webpack enviroment file
    When try to do 'rails assets:precompile', raise an enviroment
    error, and specifies some node configuration error. Remove this
    settings and add the custom config, solves the error. Finally
    add 'babel' dependencie to supress warning in Heroku.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,27 @@
 const { environment } = require('@rails/webpacker')
 
+console.log(environment.output_path)
+console.log(environment.source_path)
+console.log(environment.toWebpackConfig())
+
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  }
+};
+
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
+
+environment.config.merge(customConfig);
+
 module.exports = environment
