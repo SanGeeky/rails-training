0d030fe6b8f90be6d7cd5135a771c46c6ed2e303
David Pinchao <sangeeky@outlook.com>
2021-04-09 20:07:03 -0500

    Reformating Models with rubocop code style.

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,6 +1,6 @@
 module Visible
   extend ActiveSupport::Concern
-  VALID_STATUSES = ['public', 'private', 'archived']
+  VALID_STATUSES = %w[public private archived]
 
   included do
     validates :status, inclusion: { in: VALID_STATUSES }
@@ -11,7 +11,7 @@ module Visible
       where(status: 'public').count
     end
   end
-  
+
   def archived?
     status == 'archived'
   end
