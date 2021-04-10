562e68a84ee35cc3ce549898284e72a672f90f9f
David Pinchao <sangeeky@outlook.com>
2021-04-09 20:04:32 -0500

    Reformating code on comments controller.
    - Filters used to find comments and articles individualy
    -  Find article to create comments
    - Finding a comment to destroy the respective comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,32 @@
 class CommentsController < ApplicationController
-  
-  http_basic_authenticate_with name: "user", password: "secret", only: :destroy 
-  
-  def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comments_params)
+  before_action :find_article, only: %i[create]
+  before_action :find_comment, only: %i[destroy]
 
+  http_basic_authenticate_with name: 'user', password: 'secret', only: :destroy
+
+  def create
+    @comment = @article.comments.create(comments_params)
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
-
-    redirect_to(article_path(@article))
+    if @comment.article_id == params[:article_id].to_i
+      @comment.destroy
+    end
+    redirect_back fallback_location: articles_url
   end
-
-  private 
+  
+  private
 
   def comments_params
     params.require(:comment).permit(:commenter, :body, :status)
+  end
+
+  def find_article
+    @article = Article.find(params[:article_id])
+  end
+
+  def find_comment
+    @comment = Comment.find(params[:id])
   end
 end
