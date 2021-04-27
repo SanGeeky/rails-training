f6c89d9f41daef19d74bbb34eb3aa68d22be48ff
David Pinchao <sangeeky@outlook.com>
2021-04-26 23:53:21 -0500

    Add Application Helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,9 @@
 # application_controller.rb
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :user_logged_in?
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def user_logged_in?
-    current_user
-  end
 
   def authorized
-    redirect_to signin_path unless user_logged_in?
+    redirect_to signin_path unless session[:user_id]
   end
 
   def not_found
