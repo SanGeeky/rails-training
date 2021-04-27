5f96e5efa75d45f7163f137bd166e0a3e4f0a0d8
David Pinchao <sangeeky@outlook.com>
2021-04-27 01:55:53 -0500

    Add One to Many Relation in User and Articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@
 class User < ApplicationRecord
   attr_accessor :password
 
+  has_many :articles, dependent: :destroy
+
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
