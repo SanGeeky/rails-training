1617c280575a30c0259a77c9e474f165b9d321a0
David Pinchao <sangeeky@outlook.com>
2021-04-28 02:43:39 -0500

    Add error flash messages to signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,18 @@
 
 # sessions_controller.rb
 class SessionsController < ApplicationController
+  before_action :logged_in, only: :new
   before_action :authenticate_user, only: %i[create]
 
   def create
     session[:user_id] = @user.id
+    flash[:notice] = 'Welcome Back to Articles on Rails'
     redirect_to params[:previous_url]
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:notice] = 'See you soon on Rails!'
     redirect_back fallback_location: root_path
   end
 
@@ -18,8 +21,10 @@ class SessionsController < ApplicationController
 
   def authenticate_user
     @user = User.find_by email: params[:email]
+    flash[:error] = 'Email not found on Rails!'
     return redirect_to signin_path if @user.nil?
 
+    flash[:error] = "Incorrect password for user: #{@user.username}."
     @user.authenticate(params[:password]) ? @user : redirect_to(signin_path)
   end
 end
