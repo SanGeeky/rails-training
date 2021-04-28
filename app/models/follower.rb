9881fd92358dcf4bd370eb1cf44de0636d4620d7
David Pinchao <sangeeky@outlook.com>
2021-04-28 10:22:37 -0500

    Add relation Followers

---
+++ app/models/follower.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# follower.rb
+class Follower < ApplicationRecord
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_220650) do
+ActiveRecord::Schema.define(version: 2021_04_28_151759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2021_04_27_220650) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "followers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "follower"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_followers_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
@@ -50,4 +58,5 @@ ActiveRecord::Schema.define(version: 2021_04_27_220650) do
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
+  add_foreign_key "followers", "users"
 end
